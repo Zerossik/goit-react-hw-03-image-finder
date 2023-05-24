cb8955e5ff108e6299721f764ce430ccdb3756d5
Zerossik <zeross19940303@gmail.com>
2023-05-25 00:57:15 +0300

    Gallery

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -2,9 +2,9 @@ import { Backdrop, ModalWrap } from './Modal.styled';
 import React from 'react';
 import { createPortal } from 'react-dom';
 
-export function Modal({ children }) {
+export function Modal({ children, handleBackdropClick }) {
   return createPortal(
-    <Backdrop>
+    <Backdrop onClick={handleBackdropClick}>
       <ModalWrap>{children}</ModalWrap>
     </Backdrop>,
     document.getElementById('modal')
